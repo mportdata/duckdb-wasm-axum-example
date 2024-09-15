4b0966fca9f2a18cf258668c117a634c01acd66f
mikexporter <mikexporter@gmail.com>
2024-09-15 19:51:14 +0100

    modularised the backend

---
+++ src/modules/handlers.rs
@@ -0,0 +1,5 @@
+use crate::modules::types::templates::IndexTemplate;
+
+pub async fn index() -> IndexTemplate {
+    IndexTemplate { }
+}
---
+++ src/modules/types/templates.rs
@@ -0,0 +1,5 @@
+use askama_axum::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+pub struct IndexTemplate;
